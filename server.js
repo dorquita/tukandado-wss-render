83f600e4dbaad833ebb9c0acfd9512cb5f950501
Pau Dorca Oliver <pau.dorca1@gmail.com>
2026-03-02 15:19:03 +0100

    update

--- server.js
+++ server.js
@@ -1,29 +1,46 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import http from "http";
 import express from "express";
 import { WebSocketServer } from "ws";
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("WS alive");
-});
+app.use(express.json());
 
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
+let currentClient = null;
+
 wss.on("connection", (ws) => {
-  ws.on("message", (msg) => {
-    if (msg.toString() === "ping") {
+  console.log("Client connected");
+  currentClient = ws;
+
+  ws.on("message", (message) => {
+    const msg = message.toString();
+    console.log("Received:", msg);
+
+    if (msg === "ping") {
       ws.send("pong");
     }
   });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    currentClient = null;
+  });
 });
 
-const PORT = process.env.PORT || 3000;
+// 👇 endpoint para enviar comando desde Render
+app.post("/send", (req, res) => {
+  if (!currentClient) {
+    return res.status(400).json({ error: "No client connected" });
+  }
 
-server.listen(PORT, () => {
-  console.log("Running on port", PORT);
+  const { message } = req.body;
+  currentClient.send(message);
+
+  res.json({ sent: message });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
 });
