a6ebb88dba9951a4e47d29feed0af9e1a39e114b
Pau Dorca Oliver <pau.dorca1@gmail.com>
2026-03-02 14:56:11 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    const msg = message.toString();
+    console.log("Received:", msg);
+
+    if (msg === "ping") {
+      ws.send("pong");
+    } else {
+      ws.send("echo: " + msg);
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("WebSocket running on ws://localhost:3000");
